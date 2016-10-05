daf53f893d55a2e5d772d78da849d2dfa11f351b
Sumant Bagade <sumantbagade19@gmail.com>
2016-10-05 20:04:39 +0530

    added navbar

--- server.js
+++ server.js
@@ -3,22 +3,34 @@ var morgan = require('morgan');
 var path = require('path');
 
 var app = express();
-app.use(morgan('combined'));
+//app.use(morgan('combined'));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+app.use(express.static(__dirname + '/public'));
+
+app.set('views', path.join(__dirname, '/public/views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.get('/home', function (req, res) {
+  res.render('index');
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+
+app.get('/about', function (req, res) {
+  res.send('About page here');
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+
+app.get('/resume', function (req, res) {
+  res.render('Resume here');
 });
+
+
+
+
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
+  console.log(`IMAD course app listening on port` + port );
 });
