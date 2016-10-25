3c182b67c1c6e00c65e9d8a8d1758ab89b6364e4
Sumant Bagade <sumantbagade19@gmail.com>
2016-10-25 17:25:47 +0530

    testing user image

--- server.js
+++ server.js
@@ -49,8 +49,8 @@ app.get('/ui/user.jpg', function (req, res) {
   res.sendFile(path.join(__dirname,'ui','user.jpg'));
 });
 
-app.get('/ui/user1.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname,'ui','user1.jpg'));
+app.get('/ui/user1.JPG', function (req, res) {
+  res.sendFile(path.join(__dirname,'ui','user1.JPG'));
 });
 
 
@@ -63,5 +63,5 @@ app.get('/ui/user1.jpg', function (req, res) {
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
-  console.log(`IMAD course app listening on port` + port );
+  console.log(`IMAD course app listening on port ` + port );
 });
