05f037346869fc99f1a97ec6b9200c950347d24e
Sumant Bagade <sumantbagade19@gmail.com>
2016-11-05 21:54:12 +0530

    updated login

--- server.js
+++ server.js
@@ -219,6 +219,86 @@ app.get('/articles/:articleName', function (req, res) {
   
 });
 
+
+
+
+
+
+
+app.get('/check-login', function (req, res) {
+   if (req.session && req.session.auth && req.session.auth.userId) {
+       // Load the user object
+       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
+           if (err) {
+              res.status(500).send(err.toString());
+           } else {
+              res.send(result.rows[0].username);    
+           }
+       });
+   } else {
+       res.status(400).send('You are not logged in');
+   }
+});
+
+
+app.post('/login', function (req, res) {
+   var username = req.body.username;
+   var password = req.body.password;
+   
+   pool.query('SELECT * FROM "user" WHERE username = $1', [username], function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          if (result.rows.length === 0) {
+              res.status(403).send('username/password is invalid');
+          } else {
+              // Match the password
+              var dbString = result.rows[0].password;
+              var salt = dbString.split('$')[2];
+              var hashedPassword = hash(password, salt); // Creating a hash based on the password submitted and the original salt
+              if (hashedPassword === dbString) {
+                
+                // Set the session
+                req.session.auth = {userId: result.rows[0].id};
+                // set cookie with a session id
+                // internally, on the server side, it maps the session id to an object
+                // { auth: {userId }}
+                
+                res.send('credentials correct!');
+                
+              } else {
+                res.status(403).send('username/password is invalid');
+              }
+          }
+      }
+   });
+});
+
+app.post('/create-user', function (req, res) {
+   // username, password
+   // {"username": "tanmai", "password": "password"}
+   // JSON
+   var username = req.body.username;
+   var password = req.body.password;
+   var salt = crypto.randomBytes(128).toString('hex');
+   var dbString = hash(password, salt);
+   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send('User successfully created: ' + username);
+      }
+   });
+});
+
+
+
+
+
+
+
+
+
 app.get('/resume', function (req, res) {
   res.send('Resume here');
 });
@@ -269,6 +349,11 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/readroomscript.js', function (req, res) {
   res.sendFile(path.join(__dirname,'ui','readroomscript.js'));
 });
+
+app.get('/ui/login.js', function (req, res) {
+  res.sendFile(path.join(__dirname,'ui','login.js'));
+});
+
 
 
 app.get('/ui/readroomjquery.min.js', function (req, res) {
