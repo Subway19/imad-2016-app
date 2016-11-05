394a9c199bde85ea764c62aaa4ebc923253cdf5b
Sumant Bagade <sumantbagade19@gmail.com>
2016-11-05 23:17:18 +0530

    added logout

--- server.js
+++ server.js
@@ -305,6 +305,11 @@ app.post('/create-user', function (req, res) {
    });
 });
 
+app.get('/logout', function (req, res) {
+   delete req.session.auth;
+   res.send('<html><body>Logged out!<br/><br/><a href="/">Back to home</a></body></html>');
+});
+
 
 
 
